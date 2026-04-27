b6ea54b4d014ca412132a33d7700c85489f937db
Skylar Grow <dylangrow@gmail.com>
2026-04-26 21:08:19 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "journal-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  if (req.mode === "navigate") {
+    event.respondWith(fetch(req).catch(() => caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+    return res;
+  }).catch(() => caches.match(req))));
+});
